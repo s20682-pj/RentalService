1dd888a8b3823b7a8bef3b2d8cfe290d22a817ac
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2021-06-06 17:40:23 +0200

    Initial commit

---
+++ src/main/java/pl/pjatk/pjatk/Category.java
@@ -0,0 +1,8 @@
+package pl.pjatk.demo;
+
+public enum Category {
+    Horror,
+    History,
+    Fantasy,
+    Comedy
+}
---
+++ src/main/java/pl/pjatk/pjatk/Movie.java
@@ -0,0 +1,60 @@
+package pl.pjatk.demo;
+
+public class Movie {
+    private Long id;
+    private String name;
+    private Category category;
+    private String year;
+    private boolean isAvailable;
+
+    public Movie() {
+    }
+
+    public Movie(Long id, String name, Category category, String year, boolean isAvailable) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.year = year;
+        this.isAvailable = isAvailable;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+}
