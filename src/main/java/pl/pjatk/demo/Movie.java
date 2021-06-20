3fd674bfe8641a5ca48d0ad38888ba19cf315bbf
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2021-06-20 17:15:41 +0200

    zjazd 8

--- src/main/java/pl/pjatk/demo/Movie.java
+++ src/main/java/pl/pjatk/demo/Movie.java
@@ -1,10 +1,17 @@
 package pl.pjatk.demo;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class Movie {
+    @ApiModelProperty(notes = "This is an id of the movie", required = true, value = "id", example = "1")
     private Long id;
+    @ApiModelProperty(notes = "This is a name of the movie", required = true, value = "name", example = "xd")
     private String name;
+    @ApiModelProperty(notes = "This is a category id of the movie", required = true, value = "category", example = "Horror")
     private Category category;
+    @ApiModelProperty(notes = "This is a year id of the movie", required = true, value = "year", example = "2020")
     private String year;
+    @ApiModelProperty(notes = "This is an availability of the movie", required = true, value = "available", example = "1")
     private boolean isAvailable;
 
     public Movie() {
--- src/main/java/pl/pjatk/demo/RestController.java
+++ src/main/java/pl/pjatk/demo/RestController.java
@@ -1,5 +1,9 @@
 package pl.pjatk.demo;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,8 +17,26 @@ public class RestController {
         this.rentingService = rentingService;
     }
 
+    @ApiOperation(value =  "Find movie by given ID",
+    response = Movie.class,
+    notes = "Getting movie from provided id")
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Fine"),
+            @ApiResponse(code = 404, message = "Movie not found"),
+            @ApiResponse(code = 401, message = "You shouldn't see this"),
+            @ApiResponse(code = 403, message = "Nope")
+    })
+
     @GetMapping("/{id}")
-    public ResponseEntity<Movie> findById(@PathVariable Long id) {
+    public ResponseEntity<Movie> findById(
+            @ApiParam(name = "id",
+            type = "long",
+            value = "100",
+            example = "1",
+            required = true,
+            defaultValue = "1")
+            @PathVariable Long id) {
         return ResponseEntity.ok(rentingService.findById(id));
     }
 
